b3066ae77a9d8104288234ea3c0633e040fa1ce4
Andrey <andruhanishe@yandex.ru>
2019-05-18 11:52:18 +0300

    Test added

--- src/js/lib.js
+++ src/js/lib.js
@@ -79,3 +79,17 @@ export function countPurchases(purchases) {
 
     return count;
 }
+
+export function calculateBonus(sales, bonusPercent, minSaleForBonus) {
+    let total = 0;
+
+    for (const sale of sales) {
+        if (sale > minSaleForBonus){
+            const sum = sale - minSaleForBonus;
+            const bonus = sum * bonusPercent;
+            total += bonus;
+        }
+    }
+
+    return total;
+}
