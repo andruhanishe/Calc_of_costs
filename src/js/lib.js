e2ae36e3d7b34f0e86d7e42e769475ea6faa2320
Andrey <andruhanishe@yandex.ru>
2019-05-20 10:41:51 +0300

    Run without jest

--- src/js/lib.js
+++ src/js/lib.js
@@ -80,16 +80,16 @@ export function countPurchases(purchases) {
     return count;
 }
 
-export function calculateBonus(sales, bonusPercent, minSaleForBonus) {
-    let total = 0;
-
-    for (const sale of sales) {
-        if (sale > minSaleForBonus){
-            const sum = sale - minSaleForBonus;
-            const bonus = sum * bonusPercent;
-            total += bonus;
-        }
-    }
-
-    return total;
-}
+// export function calculateBonus(sales, bonusPercent, minSaleForBonus) {
+//     let total = 0;
+//
+//     for (const sale of sales) {
+//         if (sale > minSaleForBonus){
+//             const sum = sale - minSaleForBonus;
+//             const bonus = sum * bonusPercent;
+//             total += bonus;
+//         }
+//     }
+//
+//     return total;
+// }
