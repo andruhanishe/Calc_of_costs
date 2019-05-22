c5ad61dc232ab5e7c86b8883a07d4cb76e4365d9
Andrey <andruhanishe@yandex.ru>
2019-05-22 10:51:52 +0300

    Add test for run Travis

--- src/js/__tests__/lib.test.js
+++ src/js/__tests__/lib.test.js
@@ -1,14 +1,14 @@
-// import {calculateBonus} from "../lib.js";
-//
-//
-// test('it should calculate bonus', () => {
-//     const sales = [12000, 8000, 10000, 15000, 2000];
-//     const bonusPercent = 0.05;
-//     const minSaleForBonus = 10000;
-//
-//     const expected = 350;
-//
-//     const result = calculateBonus(sales, bonusPercent, minSaleForBonus);
-//
-//     expect(result).toBe(expected);
-// });
+import {calculate} from "../lib.js";
+
+
+ test('it should calculate bonus', () => {
+     const a = 10;
+     const b = 20;
+
+     const expected = 30;
+
+     const result = calculate(a, b);
+
+     expect(result).toBe(expected);
+});
+
